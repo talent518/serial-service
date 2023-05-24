e29541cbc9eb43d1d8fd60c800407f79f853a6e7
阿宝 <talent518@yeah.net>
2023-05-24 21:15:45 +0800

    Optimize for command line parameter.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const serial = require("serialport");
-const fs = require('fs');
+const serial = require('serialport');
+const args = require('minimist')(process.argv.slice(2));
 const net = require('net');
-const iconv = require('iconv-lite');
-const config = require('./config');
 const conns = [];
+const config = {
+	path: '/dev/ttyUSB0',
+	baudRate: 115200,
+	port: 3888,
+	isHex: false,
+};
+
+if(args.path) config.path = args.path;
+if(args.rate) config.baudRate = parseInt(args.rate);
+if(args.port) config.port = parseInt(args.port);
+if(args.hex) config.isHex = true;
+
 const server = net.createServer(function(conn) {
 	conns.push(conn);
 	conn.on('data', function(data) {
@@ -16,26 +26,32 @@ const server = net.createServer(function(conn) {
 	});
 });
 const serialPort = new serial.SerialPort({
-	path: config.com,
+	path: config.path,
 	baudRate: config.baudRate,
 	autoOpen: false
 });
 
 serialPort.open(function(e) {
 	if(e) {
-		console.log('open serial '  + config.com + ' failure', e);
+		console.log('open serial '  + config.path + ' failure', e);
 	} else {
-		console.log('open serial '  + config.com + ' success');
+		console.log('open serial '  + config.path + ' success');
 	}
 	console.log('');
 });
 
 serialPort.on('data', function(data) {
-	console.log('<', data.toString());
-	// process.stdout.write(data);
+	if(config.isHex) console.log(data.toString('hex'));
+	else process.stdout.write(data);
+
 	conns.forEach(function(conn, i) {
 		conn.write(data);
 	});
+});
+
+serialPort.on('error', function(e) {
+	console.error(e);
+	process.exit();
 });
 
 serialPort.on('close', function() {
@@ -43,9 +59,8 @@ serialPort.on('close', function() {
 });
 
 process.stdin.on('data', function(data) {
-	serialPort.write(data/*.toString().trim()+'\n'*/, function(e) {
+	serialPort.write(config.isHex ? Buffer.from(data, 'hex') : data, function(e) {
 		if(e) console.error(e);
-		else console.log('>', data.toString());
 	});
 });
 
@@ -55,4 +70,5 @@ server.listen(config.port, function() {
 
 process.on('uncaughtException', function (e) {
 	console.error(e);
+	process.exit();
 });
